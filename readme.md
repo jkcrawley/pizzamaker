8b199c80e51af15549cd84f24684d218e603cd71
James <crawleydesign@hotmail.com>
2024-02-07 02:08:05 -0600

    Version 0.3

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
+# Version 0.3
+
+## Completed
+- Added functionality
+- added prices and calcations
+
+## WIP
+-Unable to remove extra cheese price after being selected
+-Extra cheese won't display on price listing html. 
+
+
 # Version 0.2
 
 ## Completed
--- scripts/main.js
+++ scripts/main.js
@@ -1,154 +1,161 @@
 //prices
-var total = 0.00;
 
 
+const pizza = {
+    _sizes: {
+        size: '',
+        sizePrice: 0
+    },
+    _cheese: 0,
+    toppings: [],
+    set size(pizzaSize){
+            this._sizes.size = pizzaSize;
+    },
+    set basePrice(sizeCost){
+        this._sizes.sizePrice = sizeCost;
+    },
+    set cheese(extra){
+        this._cheese = extra;
+    },
+    total(){
+        var toppingsTotal = 0;
 
-//pizza size
+        for(let i = 0; i < this.toppings.length; i++){
+            toppingsTotal += this.toppings[i].cost; 
+        }
+
+        let sizePrice = this._sizes.sizePrice;
+        let totalPrice = sizePrice + this._cheese + toppingsTotal;
+        let taxes = (totalPrice * .06) + totalPrice;
+        let finalPrice = taxes;
+        return finalPrice.toFixed(2);
+    }
+}
+
+//Pizza Selection
+function pizzaSize(sizeSelect, price){
+    
+    pizza.size = sizeSelect;
+    pizza.basePrice = price;
+
+    document.getElementById('pizza-size').innerHTML = '<b>' + pizza._sizes.size + ' Pizza</b>: ' + pizza._sizes.sizePrice;
+    document.getElementById('cheeseSelect').style.display="block";
+}
 
 
+//Cheese Selection
 
+function cheese(extraChs){
+       
+    pizza.cheese = extraChs;
+    
+    document.getElementById('toppingsSelect').style.display='block';
+}
 
 //toppings
 
-function blackOlive(){
-    
 var blackolive = document.getElementById('blackolive');
+var greenolive = document.getElementById('greenolive');
+var canadianbacon = document.getElementById('canadianbacon');
+var chicken = document.getElementById('chicken');
+var jalopenopeppers = document.getElementById('jalopenopeppers');
+var onion = document.getElementById('onion');
+var pepperoni = document.getElementById('pepperoni');
+var sausage = document.getElementById('sausage');
+var tomato = document.getElementById('tomato');
 
-if(blackolive.checked == true){
-    document.querySelector(".blackolive").style.display="block";
-} else {
-    document.querySelector(".blackolive").style.display="none";
+//Create array outside of object for display purposes
+var toppingArr = [];
+
+
+function blackOlive(type, cost){
+
+    //check to see if items alreadys exists in both arrays 
+    let itemIndex = pizza.toppings.findIndex(item => item.type == type);
+    let arrIndex = toppingArr.findIndex(item => item == type + ' $' + cost)
+
+    //check if ingredient box has been selected.
+    if(blackolive.checked == true){
+        //if true: Display items on page and push to both pizza object and array.
+        document.querySelector(".blackolive").style.display="block";
+        pizza.toppings.push({type: type, cost: cost});
+        toppingArr.push(type + ' $' + cost.toFixed(2));
+    } else {
+        //if cbox isn't checked, remove item image and remove item from both object and array.
+        document.querySelector(".blackolive").style.display="none";
+        pizza.toppings.splice(itemIndex, 1);
+        toppingArr.splice(arrIndex, 1);
+    }
+
 }
-}
 
 
-function greenOlive(){
-    
-    var greenolive = document.getElementById('greenolive');
-    
+//proceed to add and remove each topping
+function greenOlive(type, cost){
+    let itemIndex = pizza.toppings.findIndex(item => item.type == type);
+    let arrIndex = toppingArr.findIndex(item => item == type + ' $' + cost)
+
     if(greenolive.checked == true){
         document.querySelector(".greenolive").style.display="block";
+        pizza.toppings.push({type: type, cost: cost});
+        toppingArr.push(type + ' $' + cost.toFixed(2));
     } else {
         document.querySelector(".greenolive").style.display="none";
+        pizza.toppings.splice(itemIndex, 1);
+        toppingArr.splice(arrIndex, 1);
     }
+
 }
 
 
-function canadianbacon(){
-    
-    var canadianbacon = document.getElementById('canadianbacon');
-        
+function cbacon(type, cost){
+    let itemIndex = pizza.toppings.findIndex(item => item.type == type);
+    let arrIndex = toppingArr.findIndex(item => item == type + ' $' + cost)
+
     if(canadianbacon.checked == true){
-            document.querySelector(".canadianbacon").style.display="block";
-            toppingPrice += .50;
-        } else {
-            document.querySelector(".canadianbacon").style.display="none";
-            if(toppingPrice >= .50){
-                toppingPrice -= .50;
-            }
-        }
-        total = total + toppingPrice;
+        document.querySelector(".canadianbacon").style.display="block";
+        pizza.toppings.push({type: type, cost: cost});
+        toppingArr.push(type + ' $' + cost.toFixed(2));
+    } else {
+        document.querySelector(".canadianbacon").style.display="none";
+        pizza.toppings.splice(itemIndex, 1);
+        toppingArr.splice(arrIndex, 1);
     }
 
-function chicken(){
-    
-    var chicken = document.getElementById('chicken');
-            
+}
+
+
+function gChicken(type, cost){
+    let itemIndex = pizza.toppings.findIndex(item => item.type == type);
+    let arrIndex = toppingArr.findIndex(item => item == type + ' $' + cost)
+
     if(chicken.checked == true){
         document.querySelector(".chicken").style.display="block";
-        toppingPrice += .50;
-        } else {
-            document.querySelector(".chicken").style.display="none";
-            if(toppingPrice >= .50){
-                toppingPrice -= .50;
-            }
-        }
-    total = total + toppingPrice;
+        pizza.toppings.push({type: type, cost: cost});
+        toppingArr.push(type + ' $' + cost.toFixed(2));
+    } else {
+        document.querySelector(".chicken").style.display="none";
+        pizza.toppings.splice(itemIndex, 1);
+        toppingArr.splice(arrIndex, 1);
+    }
+
 }
 
 
-function jpeppers(){
-    
-    var jpeppers = document.getElementById('jalopenopeppers');
-                
-    if(jpeppers.checked == true){
-        document.querySelector(".jalopenopeppers").style.display="block";
-        toppingPrice += .50;
-       } else {
-        document.querySelector(".jalopenopeppers").style.display="none";
-        if(toppingPrice >= .50){
-            toppingPrice -= .50;
-        }
-    }
-    total = total + toppingPrice;
-   }
 
-function onion(){
-    
-    var onion = document.getElementById('onion');
-                    
-    if(onion.checked == true){
-        document.querySelector(".onion").style.display="block";
+
+
+//Display prices and total
+function totalFunc(){
+
+
+
+    if(toppingArr.length === 0){
+        document.getElementById('itemList').innerHTML = ''
     } else {
-        document.querySelector(".onion").style.display="none";
+        document.getElementById('itemList').innerHTML = '<b>Toppings</b><br />' + toppingArr.join('<br/>');
     }
-}
+    document.getElementById('total').innerHTML = ' ' + pizza.total();
 
-
-function pepperoni(){
-    
-    var pepperoni= document.getElementById('pepperoni');
-                        
-    if(pepperoni.checked == true){
-        document.querySelector(".pepperoni").style.display="block";
-    } else {
-        document.querySelector(".pepperoni").style.display="none";
-       }
-    }
-var sausageBox = document.getElementById('sausage');
-
-function sausage(){
-    
-    
-                            
-    if(sausageBox.checked == true){
-        document.querySelector(".sausage").style.display="block";
-    } else {
-        document.querySelector(".sausage").style.display="none";
-    }
-    }
-
-
-var tomatoBox = document.getElementById('tomato');
-
-function tomato(){
-    
-                                
-    if(tomatoBox.checked === true){
-        document.querySelector(".tomato").style.display="block";
-    } else {
-        document.querySelector(".tomato").style.display="none";
-    }
-}
-
-
-function totalFunc(pizzaSize, toppingPrice){
-    let sizePrice = 0;
-
-    if(pizzaSize === 'small'){
-        sizePrice = 8.99;
-    } else if(pizzaSize === 'medium'){
-        sizePrice = 12.99;
-    } else if(pizzaSize === 'large'){
-        sizePrice = 16.99;
-    } else {
-        sizePrice = 0;
-    }
-
-    if(pizzaSize === 'small' && sizePrice > 8.99){
-        sizePrice -= 8.99;
-    }
-
-    total = total + sizePrice;
-    document.getElementById('total').innerHTML = " $" + total;
+    console.log(pizza._cheese);
 }
